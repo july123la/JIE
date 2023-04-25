dda37c265529e445f99fc6b4669da820feba35f1
Isabella Pacheco <131775733+Isabella-Pacheco@users.noreply.github.com>
2023-04-25 08:48:04 -0500

    Update math_game.py

--- math_game.py
+++ math_game.py
@@ -3,4 +3,17 @@ def add(num_1, num_2):
     print(f'{num_1} + {num_2} is equal to {result}')
     return result
 
-print(add(5,5))
+def substract(num_1, num_2):
+    result = num_1 - num_2
+    print(f'{num_1} - {num_2} is equal to {result}')
+    return result
+    
+def multiply(num_1, num_2):
+    result = num_1 * num_2
+    print(f'{num_1} * {num_2} is equal to {result}')
+    return result
+    
+def divide(num_1, num_2):
+    result = num_1 / num_2
+    print(f'{num_1} / {num_2} is equal to {result}')
+    return result
