9c1213212dd45f18a9684f09f76f7cacdad7cd21
july123la <124895856+july123la@users.noreply.github.com>
2023-04-25 09:25:55 -0500

    Update math_game.py

--- math_game.py
+++ math_game.py
@@ -14,11 +14,10 @@ def multiply(num_1, num_2):
     return result
     
 def divide(num_1, num_2):
-    if num_2!=0
+    if num_2!=0:
         result = num_1 / num_2
         print(f'{num_1} / {num_2} is equal to {result}')
         return result
     else:
-        print("La división hecha por 0 no está definida, prueba con otro valor")
-        return NO
+        return "NO"
         
