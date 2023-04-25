de92331e66802cbdba7be68bc894c3254b0fa9fe
Isabella Pacheco <131775733+Isabella-Pacheco@users.noreply.github.com>
2023-04-25 08:47:28 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,28 @@
-from math_game import add
+from math_game import add, substract, multiply, divide
+
 def game():
     score = 0
     while True:
-        print('======== Menu ========\n1. Add')
-        operation = int(input('Choice an operation:'))
+        print('======== Menu ========\n1. Add\n2. Substract\n3. Multiply\n4. Divide')
+        operation = int(input('Choose an operation: '))
         num_1 = int(input('Enter first number: '))
         num_2 = int(input('Enter second number: '))
         answer = int(input('Enter you answer: '))
         if operation == 1:
             result = add(num_1, num_2)
+        if operation == 2:
+            result = substract(num_1, num_2)
+        if operation == 3:
+            result = multiply(num_1, num_2)
+        if operation == 4:
+            result = divide(num_1, num_2)
         if result == answer:
             score += 1
             print('Correct!!')
         else:
             print('Incorrect')
             break
-    print(f'======== Game Over ========\nYou score is {score}\nKeep going!')
+    print(f'======== Game Over ========\n You score is {score}')
+    
 game()
 
